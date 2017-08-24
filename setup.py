14710df76c6a35f008c4eb2ec88c090ddd077486
Niloofar kheirkhah <30475811+niloofarkheirkhah@users.noreply.github.com>
2017-08-24 11:59:37 +0430

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='nili',
+    version='0.1',
+    description='Nili is a Tool for Network Scan, Man in the Middle, Protocol Reverse Engineering and Fuzzing.',
+    url='https://github.com/niloofarkheirkhah/nili',
+    author='Niloofar Kheirkhah - Ehsan Mir',
+    author_email='niloofar.kheirkhah@gmail.com - ehsan.mir@icloud.com',
+    license='GPLv3',
+    classifiers=[
+        'Topic :: Security',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+    keywords='Security Test',
+)
