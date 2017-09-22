3381877f0a770d73eae050eb76cfa4b9ca843341
ehsanmir <ehsanmiir@gmail.com>
2017-09-22 08:40:04 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='nili',
-    version='0.1',
+    version='0.1.1',
     description='Nili is a Tool for Network Scan, Man in the Middle, Protocol Reverse Engineering and Fuzzing.',
     url='https://github.com/niloofarkheirkhah/nili',
-    download_url ='https://github.com/niloofarkheirkhah/nili/archive/0.1.zip',
+    download_url ='https://github.com/niloofarkheirkhah/nili/archive/0.1.1.zip',
     author='Niloofar Kheirkhah - Ehsan Mir',
     author_email='niloofar.kheirkhah@gmail.com - ehsan.mir@icloud.com',
     license='GPLv3',
